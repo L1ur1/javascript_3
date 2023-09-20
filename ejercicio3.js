a7af9cd7271990ee1c1fe090ae7f09665fad2c4b
Laura Vargas <tinidomin58@gmail.com>
2023-09-19 22:14:09 -0500

    Ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,53 @@
+/*Ejercicio 1 
+var edad=prompt("Digite su edad");
+
+var mayor=18
+if (edad>=mayor){
+    alert("Usted es mayor de edad");
+} else {
+   alert("Usted es menor de edad");
+} 
+
+Ejercicio 2
+var nota1 = prompt("Digite la nota 1")
+var nota2 = prompt("Digite la nota 2")
+var nota3 = prompt("Digite la nota 2")
+var prome = parseInt(nota1) + parseInt(nota2) + parseInt(nota3);
+var div = prome / 3;
+alert("El promedio de las tres notas es: "+div);
+
+var aprobo = 6
+if (div>=aprobo){
+    alert("usted aprobo el curso");
+} else {
+    alert("Usted no aprobo el curso");
+}
+
+
+Ejercicio 3 
+var num1 = prompt("Digite un número A");
+var num2 = prompt ("Digite un número B");
+
+if (num1 > num2){
+    alert(num1 + " es mayor que " + num2);
+} else if (num1 < num2) {
+    alert(num1 + " es menor que " + num2);
+} else {
+    alert(num1 + " es igual a " + num2);
+}
+*/
+
+Ejercicio 5
+var peso = prompt("Digite su peso");
+
+if (peso< 40){
+    alert("Persona baja de peso");
+}  else if (peso >= 41 && peso <=70) {
+    alert(" persona, peso, promedio ");
+}   else {
+    alert(" Persona con sobrepeso ");
+} 
+
+
+
+
